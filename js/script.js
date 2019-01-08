27e126f545c12f6be3e599a19404989f961b4214
hantarr01 <thunderbolt655@mail.ru>
2019-01-08 14:20:59 +0300

    0.1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// $(Start).click(function(){
+// 	$(block).animate({
+// 		left: '5vw',
+// 		top:'6vh',
+// 		height: '50vh',
+// 		width: '30vw',
+// 		opacity: 0.9,
+// 		fontSize: "16px",
+// 	});  
+// });
